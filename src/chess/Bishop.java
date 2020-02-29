f66db6deb155e50bff90b75251df6736315b7b38
radd912 <61594448+radd912@users.noreply.github.com>
2020-02-29 07:30:58 +0000

    bishop

--- src/chess/Bishop.java
+++ src/chess/Bishop.java
@@ -30,30 +30,30 @@ public class Bishop extends Piece {
         directionY = y1 < y2 ? 1 : -1;
 
         if(directionX == 1 && directionY == 1) {   //both directions are positive check both < and adding
-            for(int i = y1; y1 <= y2; ) {
+            for(int i = y1; i <= y2;i++ ) {
                 for(int j = x1; x1 <= x2 ; ++i, ++j ) {
                     canMove = pieceOnBoard.getPiece(x2,y2).getColour() != colour;
                 }
             }
         }
         else if(directionX == -1 && directionY == -1) {   //both directions are negative so we have to see why they are still > and subtract for indexes
-            for(int i = y1; y1 >= y2; ) {
+            for(int i = y1; i >= y2; i++ ) {
                 for(int j = x1; x1 >= x2 ; --i, --j ) {
                     canMove = pieceOnBoard.getPiece(x2,y2).getColour() != colour;
                 }
             }
         }
         else if(directionX == 1 && directionY == -1) {
-            for(int i = y1; y1 >= y2; ) {
+            for(int i = y1; i>= y2; i++) {
                 for(int j = x1; x1 <= x2 ; ++i, --j ) {
                     canMove = pieceOnBoard.getPiece(x2,y2).getColour() != colour;
                 }
             }
         }
         else if(directionX == -1 && directionY == 1) {
-            for(int i = y1; y1 <= y2; ) {
+            for(int i = y1; i <= y2;i++ ) {
                 for(int j = x1; x1 >= x2 ; --i, ++j ) {
-                    canMove = pieceOnBoard.getPiece(x2,y2).getColour() != colour;
+                    canMove = pieceOnBoard.getPiece(x2, y2).getColour() != colour;
                 }
             }
         }
--- src/chess/Pawn.java
+++ src/chess/Pawn.java
@@ -16,11 +16,11 @@ public class Pawn extends Piece {
 
     @Override
     public boolean isLegitMove(int x1, int x2, int y1, int y2) {
-        x1 = Math.abs( x1 );
-        x2 = Math.abs( x2 );
-        y1 = Math.abs( y1 );
-        y2 = Math.abs( y2 );
+        boolean canMove = false;
 
-        return Math.abs( x1 - x2) == Math.abs( y1 - y2);
+        if(y1==y2 || Math.abs(x1-x2) == Math.abs(y1-y2)) canMove = true;
+        if (Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1)) > 1) canMove = false;
+
+        return canMove;
     }
 }
