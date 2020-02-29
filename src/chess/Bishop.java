6c4fdfc62ab2e5f04de652bb91adcc768a8c0a0d
radd912 <61594448+radd912@users.noreply.github.com>
2020-02-29 07:34:07 +0000

    j

--- src/chess/Bishop.java
+++ src/chess/Bishop.java
@@ -31,28 +31,28 @@ public class Bishop extends Piece {
 
         if(directionX == 1 && directionY == 1) {   //both directions are positive check both < and adding
             for(int i = y1; i <= y2;i++ ) {
-                for(int j = x1; x1 <= x2 ; ++i, ++j ) {
+                for(int j = x1; j <= x2 ; ++i, ++j ) {
                     canMove = pieceOnBoard.getPiece(x2,y2).getColour() != colour;
                 }
             }
         }
         else if(directionX == -1 && directionY == -1) {   //both directions are negative so we have to see why they are still > and subtract for indexes
             for(int i = y1; i >= y2; i++ ) {
-                for(int j = x1; x1 >= x2 ; --i, --j ) {
+                for(int j = x1; j >= x2 ; --i, --j ) {
                     canMove = pieceOnBoard.getPiece(x2,y2).getColour() != colour;
                 }
             }
         }
         else if(directionX == 1 && directionY == -1) {
             for(int i = y1; i>= y2; i++) {
-                for(int j = x1; x1 <= x2 ; ++i, --j ) {
+                for(int j = x1; j <= x2 ; ++i, --j ) {
                     canMove = pieceOnBoard.getPiece(x2,y2).getColour() != colour;
                 }
             }
         }
         else if(directionX == -1 && directionY == 1) {
             for(int i = y1; i <= y2;i++ ) {
-                for(int j = x1; x1 >= x2 ; --i, ++j ) {
+                for(int j = x1; j >= x2 ; --i, ++j ) {
                     canMove = pieceOnBoard.getPiece(x2, y2).getColour() != colour;
                 }
             }
